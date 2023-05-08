c97bf43263c5af4d62e08677d8fbaa09be217133
Andy Lee <a.lee.intl@gmail.com>
2023-05-08 22:21:40 +0200

    Experimenting Next-intl

---
+++ middleware.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server"
+import { match } from "@formatjs/intl-localematcher"
+import Negotiator from "negotiator"
+
+const locales = ["en", "fr", "ko"]
+const defaultLocale = "en"
+
+export function middleware(request: NextRequest) {
+  // Check if there is any supported locale in the pathname
+  const pathname = request.nextUrl.pathname
+  const pathnameIsMissingLocale = locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  )
+
+  const accept_language = request.headers.get("accept-language")
+  // Redirect if there is no locale
+  if (pathnameIsMissingLocale) {
+    const locale = accept_language
+      ? match(
+          new Negotiator({
+            headers: {
+              "accept-language": accept_language,
+            },
+          }).languages(),
+          locales,
+          defaultLocale
+        )
+      : defaultLocale
+
+    // e.g. incoming request is /products
+    // The new URL is now /en-US/products
+    return NextResponse.redirect(
+      new URL(`/${locale}${pathname ? `/${pathname}` : ""}`, request.url)
+    )
+  }
+}
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    "/((?!_next).*)",
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+}
