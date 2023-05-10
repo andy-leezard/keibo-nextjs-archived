1e2530d5a1d43392fca2f654344adca53454bb87
Andy Lee <a.lee.intl@gmail.com>
2023-05-10 17:17:23 +0200

    Add generic global utility types

--- lib/types.ts
+++ lib/types.ts
@@ -1,6 +1,17 @@
 declare global {
-  type SupportedLanguage = "en" | "fr" | "ko"
+  /** Layout */
   type SearchParams = Record<string, string>
+  type WithSearchParams = { searchParams: SearchParams }
+
+  /** Utils */
+  type ValueOf<T> = T[keyof T]
+  type PartiallyOptional<T, K extends keyof T> = Omit<T, K> &
+    Partial<Pick<T, K>>
+  type PartiallyRequired<T, K extends keyof T> = Omit<T, K> &
+    Required<Pick<T, K>>
+  type RequiredAttributes<T> = {
+    [K in keyof T as T[K] extends Required<T>[K] ? K : never]: T[K]
+  }
 }
 
 /** export any type to declare this file */
