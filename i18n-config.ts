77de7b458d556a0d5958787c8b0e1453efcb5243
Andy Lee <a.lee.intl@gmail.com>
2023-05-14 00:54:57 +0200

    T intl method exported directly from config.ts

--- i18n-config.ts
+++ i18n-config.ts
@@ -6,3 +6,17 @@ export const i18n = {
 export type Locale = (typeof i18n)["locales"][number]
 export type WithLocale = { currentLocale: Locale }
 export type WithLocaleParam = { params: { lang: Locale } }
+
+/** Takes a dictionary and conditionally return the corresponding output */
+export const t = (
+  locale: Locale,
+  dictionary: PartiallyRequired<
+    Partial<Record<Locale, string>>,
+    (typeof i18n)["defaultLocale"]
+  >
+):string => {
+  if (Object.prototype.hasOwnProperty.call(dictionary, locale)) {
+    return dictionary[locale] as string
+  }
+  return dictionary[i18n.defaultLocale] as string
+}
