0398f388a00dc05c775fdf08decb7ae5e1142f51
Andy Lee <a.lee.intl@gmail.com>
2023-05-20 00:24:19 +0200

    Fixed env variable naming strategy

--- lib/client/firebase/firebase.ts
+++ lib/client/firebase/firebase.ts
@@ -3,7 +3,17 @@ import { getDatabase } from "firebase/database"
 import { getStorage } from "firebase/storage"
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
-import firebaseConfig from "@/lib/firebase.config"
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  appId: process.env.FIREBASE_APP_ID,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
@@ -20,4 +30,4 @@ const fdb = getFirestore(app)
 // Initialize Auth feature and get a reference to the service
 const auth = getAuth(app)
 
-export { firebaseConfig, app, rdb, strg, fdb, auth }
+export { app, rdb, strg, fdb, auth }
--- lib/client/firebase/firestore.ts
+++ lib/client/firebase/firestore.ts
@@ -1,11 +1,40 @@
 "use client"
 
 import { isOdd } from "@/utils"
-import { DocumentData, doc, updateDoc } from "firebase/firestore"
+import { DocumentData, doc, getDoc, updateDoc } from "firebase/firestore"
 import { fdb } from "./firebase"
 
 export const docRef = (initialCollection: string, pathSegments: string[]) =>
   doc(fdb, initialCollection, ...pathSegments)
+
+export const getFirestore = (
+  initialCollection: string,
+  pathSegments: string[]
+) => getDoc(docRef(initialCollection, pathSegments))
+
+export const fetchFirestore = async <T>(
+  initialCollection: string,
+  pathSegments: string[]
+): Promise<T | null> => {
+  try {
+    if (!isOdd(pathSegments.length)) {
+      throw new Error(
+        `Firebase FDB Error: path segments' length ${pathSegments.length} is not an odd number; path is pointing a collection instead of a doc. If you wanted to fetch a whole collection, use another method.`
+      )
+    }
+    const snapshot = await getFirestore(initialCollection, pathSegments)
+    if (!snapshot.exists()) {
+      throw new Error(
+        `Firebase FDB Error: path ${initialCollection}${pathSegments.map(
+          (p) => `/${p}`
+        )} does not exist!`
+      )
+    }
+    return snapshot.data() as T
+  } catch (e) {
+    return null
+  }
+}
 
 export const updateFirestore = async (
   initialCollection: string,
