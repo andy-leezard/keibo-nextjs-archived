54b46c21ea29ab03c93b28758c1b7dafae4034e5
Andy Lee <a.lee.intl@gmail.com>
2023-05-21 01:41:05 +0200

    Create dictionary utility types

--- i18n-config.ts
+++ i18n-config.ts
@@ -6,15 +6,13 @@ export const i18n = {
 export type Locale = (typeof i18n)["locales"][number]
 export type WithLocale = { currentLocale: Locale }
 export type WithLocaleParam = { params: { lang: Locale } }
+export type PDictionary = PartiallyRequired<
+  Partial<Record<Locale, string>>,
+  (typeof i18n)["defaultLocale"]
+>
 
 /** Takes a dictionary and conditionally return the corresponding output */
-export const t = (
-  locale: Locale,
-  dictionary: PartiallyRequired<
-    Partial<Record<Locale, string>>,
-    (typeof i18n)["defaultLocale"]
-  >
-):string => {
+export const t = (locale: Locale, dictionary: PDictionary): string => {
   if (Object.prototype.hasOwnProperty.call(dictionary, locale)) {
     return dictionary[locale] as string
   }
