42f6786f08d2866e7b5b37d6ab397c9c8505330c
Andy Lee <a.lee.intl@gmail.com>
2023-05-26 13:50:08 +0200

    Add string normalizer util

--- utils/index.ts
+++ utils/index.ts
@@ -3,3 +3,16 @@ export const isValidEmailAddress = (address: string) => {
   return emailRegex.test(address)
 }
 export const isOdd = (n: number) => (n & 1) === 1
+export const normalize = (
+  str: string,
+  unifyCase?: "uppercase" | "lowercase"
+) => {
+  const res = str.normalize("NFKD").replace(/[\u0300-\u036f]/g, "")
+  if (unifyCase === "uppercase") {
+    return res.toUpperCase()
+  }
+  if (unifyCase === "lowercase") {
+    return res.toLowerCase()
+  }
+  return res
+}
