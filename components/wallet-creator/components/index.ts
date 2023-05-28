faa4446098e207166fd929f44742a05cde8aeb28
Andy Lee <a.lee.intl@gmail.com>
2023-05-28 23:50:58 +0200

    Remove unused component

--- components/wallet-creator/components/index.ts
+++ components/wallet-creator/components/index.ts
@@ -1,6 +1,5 @@
 import AssetSelection from "./AssetSelection"
 import Category from "./Category"
 import WalletProvider from "./WalletProvider"
-import Detail from "./Detail"
 
-export { AssetSelection, Category, WalletProvider, Detail }
+export { AssetSelection, Category, WalletProvider }
