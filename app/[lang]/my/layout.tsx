86c96e48e5441ad954056f9cdd5b8741cfb858a0
Andy Lee <a.lee.intl@gmail.com>
2023-07-29 19:24:26 +0200

    Add navbar to my directory layout

--- app/[lang]/my/layout.tsx
+++ app/[lang]/my/layout.tsx
@@ -1,10 +1,16 @@
 import { RequireAuth } from "@/components/utils"
 import { WithLocaleParam } from "@/i18n-config"
+import { Navbar } from "@/components/layout"
 
 interface LayoutProps extends WithLocaleParam {
   children: React.ReactNode
 }
 
 export default async function Layout({ children, params }: LayoutProps) {
-  return <RequireAuth>{children}</RequireAuth>
+  return (
+    <RequireAuth>
+      <Navbar currentLocale={params.lang} />
+      {children}
+    </RequireAuth>
+  )
 }
