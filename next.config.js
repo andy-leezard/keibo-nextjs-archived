d04bc5603b26a800c9b2e896bb6b1bf9a9b40b3d
Andy Lee <a.lee.intl@gmail.com>
2023-08-03 19:31:56 +0200

    Fix compiling errors with styled components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {},
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  },
   images: {
     domains: [
       "lh3.googleusercontent.com",
