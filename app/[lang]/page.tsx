b1710df9d2b12d044b96c91381db28ee77b5e370
Andy Lee <a.lee.intl@gmail.com>
2023-08-29 23:16:34 +0200

    SSR Homepage

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,13 +1,21 @@
 import { WithLocaleParam } from "@/i18n-config"
 import BanalceSynthesis from "@/components/balanceSynthesis"
+import { getUser } from "@/utils/common/auth"
+import { cookies } from "next/headers"
 
 type PageProps = WithLocaleParam & WithSearchParams
 
-export default function Page({ params, searchParams }: PageProps) {
+export default async function Page({ params, searchParams }: PageProps) {
+  const {
+    networkError,
+    statusCode,
+    data: user,
+  } = await getUser(cookies().toString())
+  
   return (
     <>
       <main className="relative flex flex-1 flex-col">
-        <BanalceSynthesis currentLocale={params.lang} />
+        <BanalceSynthesis currentLocale={params.lang} user={user} />
       </main>
     </>
   )
--- components/balanceSynthesis/BanalceSynthesis.tsx
+++ components/balanceSynthesis/BanalceSynthesis.tsx
@@ -1,132 +1,81 @@
 "use client"
 
-import { useLayoutEffect, useState } from "react"
+import { useState } from "react"
 import { shuffleArray } from "@/utils/client"
 import { TypingText } from "./TypingText"
 import { Button } from "../ui"
 import { WithLocale, t } from "@/i18n-config"
 import { slogans } from "./constants"
-import { ColorfulSpinner } from "../ui/loaders"
 import { useRouter } from "next/navigation"
-import Warning from "../warning/Warning"
 import Link from "next/link"
-import { useAppSelector } from "@/redux/hooks"
 
-type banalceSynthesisProps = WithLocale & {}
+type banalceSynthesisProps = WithLocale & {
+  user: SerializedUser | null
+}
 
-const BanalceSynthesis = ({ currentLocale }: banalceSynthesisProps) => {
-  const { isAuthenticated, isLoading } = useAppSelector((state) => state.auth)
-
+const BanalceSynthesis = ({ currentLocale, user }: banalceSynthesisProps) => {
   const router = useRouter()
-  const [wallets, setWallets] = useState<any | null>(null)
-  const [fetchingWallets, setFetchingWallets] = useState(true)
-  const [sessionError, setSessionError] = useState(false)
   const [textList] = useState(
     shuffleArray(slogans.map((s) => t(currentLocale, s)))
   )
-
-  useLayoutEffect(() => {
-    const fetchWallets = async () => {
-      /* if (session?.user?.id) {
-        const [kuser, error] = await fetchFirestore<KeiboFirestoreUser>(
-          "users",
-          [session.user.id]
-        )
-        if (!kuser) {
-          setSessionError(true)
-          return
-        }
-        if (kuser.wallets && process.env.NODE_ENV !== "production") {
-          return
-        }
-        const update_payload: Partial<KeiboFirestoreUser> = {
-          last_activity: new Date().toISOString(),
-          last_activity_unix: Date.now(),
-          platform: "web",
-        }
-        if (!kuser.wallets) {
-          update_payload.wallets = []
-        }
-        await updateFirestore("users", [session.user.id], update_payload)
-      } else {
-        setWallets(null)
-      } */
-    }
-    fetchWallets().then(() => setFetchingWallets(false))
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  const wallets: SerializedWallet[] = []
 
   return (
     <div className="p-4 m-auto rounded-md bg-sky-200 dark:bg-indigo-500">
       <div className="flex flex-col items-center justify-center text-center">
-        {isLoading || fetchingWallets ? (
-          <ColorfulSpinner size={64} withShadow />
-        ) : (
+        {user ? (
           <>
-            {isAuthenticated ? (
+            {wallets ? (
               <>
-                {sessionError ? (
-                  <>
-                    <Warning currentLocale={currentLocale} />
-                  </>
-                ) : wallets ? (
-                  <>
-                    <span style={{ fontSize: "2rem" }}>🚀 Your Summary 🚀</span>
-                  </>
-                ) : (
-                  <>
-                    <p>
-                      {t(currentLocale, {
-                        en: "You currently have no wallets registered",
-                        fr: "Vous n'avez actuellement aucun portefeuille enregistré",
-                        ko: "현재 등록된 지갑이 없습니다.",
-                      })}
-                    </p>
-                    <Button
-                      onPress={() => router.push("accounts/new-wallet")}
-                      style={{ marginTop: "1rem" }}
-                      corner="rounded"
-                    >
-                      {t(currentLocale, {
-                        en: "Create your first wallet 🚀",
-                        fr: "Créer mon premier wallet 🚀",
-                        ko: "지갑 생성하기 🚀",
-                      })}
-                    </Button>
-                  </>
-                )}
+                <span style={{ fontSize: "2rem" }}>🚀 Your Summary 🚀</span>
               </>
             ) : (
               <>
-                <h2>🚀 WELCOME TO KEIBO 🚀</h2>
-                <div className="flex">
-                  <span>
-                    {t(currentLocale, {
-                      en: "Your",
-                      fr: "Votre",
-                      ko: "당신의",
-                    })}
-                    {String.fromCharCode(160)}
-                  </span>
-                  <TypingText
-                    texts={textList}
-                    waitbt={50}
-                    wait={3000}
-                    speed={27}
-                  />
-                </div>
-                <Link
-                  href="/auth/login"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                <p>
+                  {t(currentLocale, {
+                    en: "You currently have no wallets registered",
+                    fr: "Vous n'avez actuellement aucun portefeuille enregistré",
+                    ko: "현재 등록된 지갑이 없습니다.",
+                  })}
+                </p>
+                <Button
+                  onPress={() => router.push("accounts/new-wallet")}
+                  style={{ marginTop: "1rem" }}
+                  corner="rounded"
                 >
                   {t(currentLocale, {
-                    en: "Sign in",
-                    fr: "Se connecter",
-                    ko: "로그인",
+                    en: "Create your first wallet 🚀",
+                    fr: "Créer mon premier wallet 🚀",
+                    ko: "지갑 생성하기 🚀",
                   })}
-                </Link>
+                </Button>
               </>
             )}
+          </>
+        ) : (
+          <>
+            <h2>🚀 WELCOME TO KEIBO 🚀</h2>
+            <div className="flex">
+              <span>
+                {t(currentLocale, {
+                  en: "Your",
+                  fr: "Votre",
+                  ko: "당신의",
+                })}
+                {String.fromCharCode(160)}
+              </span>
+              <TypingText texts={textList} waitbt={50} wait={3000} speed={27} />
+            </div>
+            <Link
+              href="/auth/login"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              {t(currentLocale, {
+                en: "Sign in",
+                fr: "Se connecter",
+                ko: "로그인",
+              })}
+            </Link>
           </>
         )}
         {process.env.NODE_ENV !== "production" ? (
