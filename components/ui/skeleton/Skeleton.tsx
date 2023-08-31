4285ff1a4c7c02ccc04eb7c6b725cbbb86cfa48a
Andy Lee <a.lee.intl@gmail.com>
2023-09-01 01:15:59 +0200

    Conditionally render skeleton

--- components/ui/skeleton/Skeleton.tsx
+++ components/ui/skeleton/Skeleton.tsx
@@ -1,6 +1,7 @@
 import cn from "classnames"
 import { PropsWithChildren } from "react"
 import { base_td_classname } from "./constants"
+import { isNullish } from "@/utils"
 
 type SkeletonProps = PropsWithChildren & {
   className?: string
@@ -10,7 +11,7 @@ const Skeleton = ({ children, className }: SkeletonProps) => {
   return (
     <div
       className={cn(
-        ...base_td_classname,
+        ...(isNullish(children) ? base_td_classname : []),
         className
       )}
     >
